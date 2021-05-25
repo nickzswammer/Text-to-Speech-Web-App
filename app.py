faa196307bb98fbf4f60e418de1112f7a2a79b94
Nicholas Zhang <zhang.nicholas136@gmail.com>
2021-05-25 13:33:00 -0700

    Adding error handlers

--- app.py
+++ app.py
@@ -23,10 +23,19 @@ def generated():
     tts = f'http://api.voicerss.org/?key=444448125f0b47b0880e164aea946d51&hl={language}&src={content}'
 
     if content == '':
-        tts = f'http://api.voicerss.org/?key={KEY}&hl={language}&src=No input detected. Please try again.'
+        tts = f'http://api.voicerss.org/?key=444448125f0b47b0880e164aea946d51&hl&hl={language}&src=No input detected. Please try again.'
         return render_template('generated.html', api=tts, content='No input detected. Please try again.')
     else:
         return render_template('generated.html', api=tts, content=content)
+
+#Error Handlers
+@app.errorhandler(500)
+def server_error():
+    return render_template('500.html')
+
+@app.errorhandler(404)
+def page_not_found(self):
+    return render_template('404.html')
 
 #Run App
 if __name__ == '__main__':
